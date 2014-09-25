5bf4e52b834acb74ef65e5c7ac9fc9b26d89dd92
DevillordMCPE <maulaxe@yahoo.com>
2014-09-25 17:11:44 +0700

    Create Ender Pearl.js

---
+++ Ender Pearl.js
@@ -0,0 +1,35 @@
+/*
+ 
+  __ModPe Script__
+  Ender Pearl
+  RedEagle Team
+  1.0.0
+
+*/
+
+function newLevel(){
+	addItemInventory(332,16);
+	clientMessage(ChatColor.RED + "[RE]" + ChatColor.GREEN +  " Snowball Added");
+}
+
+function entityRemovedHook(entity) {
+	if(Entity.getEntityTypeId(entity) == 81) {
+		onPosition(Entity.getX(entity),Entity.getY(entity),Entity.getZ(entity));
+	}
+}
+
+function onPosition(x,y,z) {
+	setPosition(getPlayerEnt(),x,y+4,z);
+}
+
+/*
+
+	Copyright Red Eagle Team 2014.
+	This Mod is Coded By Red Eagle PE Modder and Pocketmine Pluginers Team.
+	This Is Free Mod PE Scripts 
+	But you cannot redistribute,recoded,and using our code without permission from Head Of Team.
+	Head Of Team : DevilLord SoulDeath Reaper
+	Facebook : facebook.com/ville.a.mania/
+	Github : github.com/MPRedEagle/ModPe-Script/
+
+*/
